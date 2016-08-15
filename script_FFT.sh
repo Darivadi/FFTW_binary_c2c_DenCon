f915bfd5a72d6cb123073f265ecf7c13d76804cf
Darivadi <darivadi@gmail.com>
2016-08-15 13:26:38 -0500

    15-08-16 13:26

--- script_FFT.sh
+++ script_FFT.sh
@@ -1,3 +1,4 @@
 #time ./FFT_of_densities.x ./Parameter_files/parameters_file_ASCII.dat | tee screen_FFT_c2c.txt
 #time ./FFT_of_densities.x ./Parameter_files/parameters_file_Binary_512.dat | tee screen_FFT_c2c.txt
-time ./FFT_of_densities.x ./Parameter_files/parameters_file_Binary.dat | tee screen_FFT_c2c.txt
+#time ./FFT_of_densities.x ./Parameter_files/parameters_file_Binary.dat | tee screen_FFT_c2c.txt
+time ./FFT_of_densities.x ./Parameter_files/parameters_file_Binary_1024.dat | tee screen_FFT_c2c.txt
